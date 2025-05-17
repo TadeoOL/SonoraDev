0607340029b53f72d9daac59827ac277a3762f88
TadeoOL <l19330633@hermosillo.tecnm.mx>
2025-05-17 12:46:52 -0700

    fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://sonoradev.com";
+  const baseUrl = "https://sonora-dev.vercel.app";
 
   return [
     {
